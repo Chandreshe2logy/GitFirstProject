c4487d43bb6191ded8daa343eccf7ccedc6c1d57
Chandresh Patel <Chandresh@e2logy.com>
2020-05-04 12:42:27 +0530

    Test data

--- GitFirstProject/AppDelegate.swift
+++ GitFirstProject/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        print("My name is chandres patel")
         // Override point for customization after application launch.
         return true
     }
